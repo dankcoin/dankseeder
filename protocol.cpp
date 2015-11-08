2fae70d6184e64c1946d4ff5173b12e7b0d9b310
Anand Capur <me@anand.io>
2015-11-08 04:46:05 -0500

    DANKSEED

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xDE, 0xCA, 0xFE, 0xCB };
 
 CMessageHeader::CMessageHeader()
 {
